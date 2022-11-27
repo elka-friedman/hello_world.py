4289a355ff70b6163ba99adab6a735a3d3832c8b
Elka Friedman <elkafriedman1@gmail.com>
2022-11-27 11:06:09 -0500

    booleans

---
+++ boolean.py
@@ -0,0 +1,17 @@
+#5==5
+#print(5==5)
+#print(5!=5)
+#print(6<4)
+#print(4>=4)
+#print(4>4)
+#print("Giraffe" == "Giraffe")
+#print("Giraffe" == "giraffe")
+#print("Giraffe" != "Hippo")
+
+rain=True
+
+if(rain):
+    print("Use an umbrella!")
+else:
+    print("It's dry out!")
+print("Have a great day!")
